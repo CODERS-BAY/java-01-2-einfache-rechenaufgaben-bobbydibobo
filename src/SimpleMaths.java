232665a1d05d159b32b5c583fd8926b446ed6913
Robert Ardeleanu <robert.ardeleanu@gmx.at>
2020-08-27 14:19:41 +0200

    Perform simple calculations revamp

--- src/SimpleMaths.java
+++ src/SimpleMaths.java
@@ -5,15 +5,34 @@ public class SimpleMaths {
         int numberOne = 278;
         int numberTwo = 192;
 
-        int sum = (numberOne + numberTwo);
-        int difference = (numberOne - numberTwo);
-        int product =  (numberOne * numberTwo);
-        double qoutient = (double) numberOne / numberTwo;
+        int sum = numberOne + numberTwo;
+        int difference = numberOne - numberTwo;
+        int product =  numberOne * numberTwo;
+        double qoutient = numberOne / numberTwo;
 
-        System.out.println(numberOne + " + " + numberTwo + " = " + sum);
-        System.out.println(numberOne + " - " + numberTwo + " = " + difference);
-        System.out.println(numberOne + " * " + numberTwo + " = " + product);
-        System.out.format(numberOne + " / " + numberTwo + " = %.2f", qoutient);
+        System.out.printf("%d + %d = %d %n",numberOne,numberTwo,sum);
+        System.out.printf("%d - %d = %d %n",numberOne,numberTwo,difference);
+        System.out.printf("%d * %d = %d %n",numberOne,numberTwo,product);
+        System.out.printf("%d / %d = %.2f %n",numberOne,numberTwo,qoutient);
+
+
+        System.out.println("\n");
+
+
+        double numberOneD = 278;
+        double numberTwoD = 192;
+
+        double sumD = numberOneD + numberTwoD;
+        double differenceD = numberOneD - numberTwoD;
+        double productD =  numberOneD * numberTwoD;
+        double qoutientD = numberOneD / numberTwoD;
+
+
+        System.out.printf("%.0f + %.0f = %.0f %n",numberOneD,numberTwoD,sumD);
+        System.out.printf("%.0f - %.0f = %.0f %n",numberOneD,numberTwoD,differenceD);
+        System.out.printf("%.0f * %.0f = %.0f %n",numberOneD,numberTwoD,productD);
+        System.out.printf("%.0f / %.0f = %.2f %n",numberOneD,numberTwoD,qoutientD);
+
 
 
     }
